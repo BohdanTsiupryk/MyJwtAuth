c5427ee047af5c946c510d387e313d4c244462c5
bohdan.tsiupryk <bohdan.tsiupryk@eleks.com>
2020-10-21 21:13:27 +0300

    initial commit

---
+++ src/test/java/bts/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package bts.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
