7edc02f01829e3754845c4c3b65e6c587a0ba878
bohdan.tsiupryk <bohdan.tsiupryk@eleks.com>
2020-10-22 04:02:03 +0300

    fix error handling

--- src/main/java/bts/test/controler/BaseController.java
+++ src/main/java/bts/test/controler/BaseController.java
@@ -1,7 +1,14 @@
 package bts.test.controler;
 
 import bts.test.exception.BadPasswordException;
+import bts.test.exception.UserAlreadyExistException;
+import bts.test.util.DateUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,19 +24,24 @@ public class BaseController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<MetaData> handle(Exception exception) {
-        Date now = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
-        MetaData metaData = new MetaData(exception.getMessage(), now, HttpStatus.INTERNAL_SERVER_ERROR);
+        MetaData metaData = new MetaData(exception.getMessage(), DateUtil.getNowDate(), HttpStatus.INTERNAL_SERVER_ERROR);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(metaData);
     }
 
     @ExceptionHandler(BadPasswordException.class)
-    public ResponseEntity<MetaData> handle403(BadPasswordException exception) {
-        Date now = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
-        MetaData metaData = new MetaData(exception.getMessage(), now, HttpStatus.FORBIDDEN);
+    public ResponseEntity<MetaData> handle403(Exception exception) {
+        MetaData metaData = new MetaData(exception.getMessage(), DateUtil.getNowDate(), HttpStatus.FORBIDDEN);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(metaData);
     }
 
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<MetaData> handleExisting(UserAlreadyExistException exception) {
+        MetaData metaData = new MetaData(exception.getMessage(), DateUtil.getNowDate(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(metaData);
+    }
+
     @AllArgsConstructor
+    @Data
     public static class MetaData {
         private String errorMessage;
         private Date date;
